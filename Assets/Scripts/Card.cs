fed0aa48b60f661812d39a75ac30b56a329d2336
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2025-06-09 22:42:37 -0400

    Made ediitable stats for cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,11 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public int attackPower;
+    public int currentHealth;
+    public int manaCost;
+
+    public TMP_Text attackText;
+    public TMP_Text healthText;
+    public TMP_Text costText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        attackText.text = attackPower.ToString();
+        healthText.text = currentHealth.ToString();
+        costText.text = manaCost.ToString();
     }
 
     // Update is called once per frame
