4521bf9b422e04af2d365e4db2b3541568791b3c
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2025-07-13 21:32:14 -0400

    CardsScriptableObject for creating cards implemented. Better for memory and faster development

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,11 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject cardSO;
+
     public int attackPower;
     public int currentHealth;
     public int manaCost;
@@ -10,13 +13,33 @@ public class Card : MonoBehaviour
     public TMP_Text attackText;
     public TMP_Text healthText;
     public TMP_Text costText;
+    public TMP_Text nameText, actionDescriptionText, loreText;
+
+    public Image characterArt, bgArt;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        attackText.text = attackPower.ToString();
+        SetupCard();
+    }
+
+    public void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
         healthText.text = currentHealth.ToString();
+        attackText.text = attackPower.ToString();
         costText.text = manaCost.ToString();
+        //UpdateCardDisplay();
+
+        nameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        bgArt.sprite = cardSO.bgSprite;
     }
 
     // Update is called once per frame
